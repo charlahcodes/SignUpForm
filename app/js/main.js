07b9df72333458440234170159ca69b49f6abba9
Josh Brzinski <jbbrzinski@gmail.com>
2015-10-13 14:28:27 -0400

    HTML Framework Updated; js started for language selector.

--- app/js/main.js
+++ app/js/main.js
@@ -5,28 +5,43 @@
   console.log('It Works!');
 })();
 
-
-(function formjson (keyword) {
+(function formjson(keyword) {
   var promise = $.ajax({
     url: 'https://json-data.herokuapp.com/forms',
     dataType: 'json',
     method: 'get',
-    success: function(data) {
-      console.log('cb', data)
-    },
+    success: function success(data) {
+      console.log('cb', data);
+    }
   });
 
-  promise.then(function(data){
+  promise.then(function (data) {
     var templateString = $('#itemTemplate').text();
+    var templateStringSelect = $('#itemSelector').text();
     var renderTemplate = _.template(templateString);
-    _.each(data, function(attrib) {
-      console.log('attrib',attrib);
-      var freshHTML = renderTemplate(attrib);
-      $('.container').append(freshHTML);
+    var renderSelector = _.template(templateStringSelect);
+
+    _.each(data, function (attrib) {
+
+      // var selectHTML = renderSelector(attrib)
+      // var SelectOption = $('#selectOption')
+      // var renderOption = _.template(selectOption)
+      // var SelOptions = attrib.options
+
+      // _.each(lang,function {
+      //     $('<select>').append(renderOption)
+      //   });
+
+      console.log('attrib', attrib);
+
+      if (attrib.options > 0 && type === 'select') {
+        $('container').append(selectHTML);
+      } else {
+        var freshHTML = renderTemplate(attrib);
+        $('.container').append(freshHTML);
+      }
     });
-      console.log('pr', data);
-    });
+  });
 
   return promise;
-}());
-
+})();
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
+'use strict';
+
 (function () {
 
   console.log('It Works!');
-}())
+})();
 
 
 (function formjson (keyword) {
   var promise = $.ajax({
     url: 'https://json-data.herokuapp.com/forms',
-    dataType: 'jsonp',
+    dataType: 'json',
     method: 'get',
     success: function(data) {
       console.log('cb', data)
@@ -15,8 +17,32 @@
   });
 
   promise.then(function(data){
-      console.log('pr', data);
+    var templateString = $('#itemTemplate').text();
+    var templateStringSelect = $('#itemSelector').text();
+    var renderTemplate = _.template(templateString);
+    var renderSelector = _.template(templateStringSelect);
+
+    _.each(data, function(attrib) {
+      
+      // var selectHTML = renderSelector(attrib)
+      // var SelectOption = $('#selectOption')
+      // var renderOption = _.template(selectOption)
+      // var SelOptions = attrib.options
+      
+      // _.each(lang,function {
+      //     $('<select>').append(renderOption)
+      //   });
+
+      console.log('attrib',attrib);
+
+      if(attrib.options > 0 && type === 'select') {
+        $('container').append(selectHTML);
+      } else {
+        var freshHTML = renderTemplate(attrib);
+        $('.container').append(freshHTML);
+      }
     });
+  });
 
   return promise;
-}());
+}())
